e5f2eb436e37b0eb4a97b8f0011f68cd6e1db5d5
jgstratton <jgstratt@sdf.org>
2019-01-26 14:51:01 -0600

    Switched configs to settings. Removed some configs.

--- README.md
+++ README.md
@@ -1,30 +1,20 @@
-# MQTT for MycroftAI
-
+# <img src='https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/robot.svg' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/> MQTT for MycroftAI
 This is a skill written for mycroft to publish commands over an mqtt broker for home automation or any other purpose.
 
-# Commands
-
+## About
 Currently it will publish the action to a topic built from the commands said, for example
 - say `hey mycroft, turn the light on` and mycroft will publish `on` to `/mycroft/turn/light/on`.
 - say `hey mycroft, switch the tv on` and mycroft will publish `on` to `/mycroft/switch/tv/on`.
 
-# Installing the skill
+## Examples
+* "Turn the light on."
+* "Switch the tv on."
 
-This skill requires paho-mqtt to be installed in your mycroft virtual enviroment.
+## Credits
+@jamiehoward430
 
-If you already have mycroft running stop it and enter the following commands,
-- to enter the virtual enviroment `workon mycroft`.
-- now to install paho-mqtt `pip install paho-mqtt`.
-- now you can exit the virtual enviroment by entering `deactivate`
-
-If you have not yet setup mycroft then simply add `paho-mqtt==1.1` to the requirments.txt file before running dev_setup.sh
-
-Now you have paho-mqtt installed move the mycroft skills directory and download the skill.
-- Move into the directory `cd ~/mycroft-core/mycroft/skills`
-- Clone the repository `git clone https://github.com/jamiehoward430/mycroft-mymqtt.git`
-- Rename it `mv mycroft-mymqtt mymqtt`
-
-# Setup mycroft.conf
+## Notes
+### Setup mycroft.conf
 Add the following lines to your config file and adjust to your needs, if you ar not using SSL or authentication
 just change them to no.
 If you are using SSL and a self signed certificate you will need to change mqtt-ca-cert to the location of your cerftificate,
@@ -46,3 +36,13 @@ Or you can add it the default trusted certificates.
   }
 ```
 Thas it, now start mycroft and start turning your light on and off.
+
+## Supported Devices
+platform_picroft platform_plasmoid 
+
+## Category
+IoT
+
+## Tags
+#mqtt
+
--- __init__.py
+++ __init__.py
@@ -4,10 +4,9 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
-from urllib2 import urlopen
 import paho.mqtt.client as mqtt
 
-__author__ = 'jamiehoward430'
+__author__ = 'jamiehoward430/JonStratton'
 
 LOGGER = getLogger(__name__)
 
@@ -15,21 +14,16 @@ class mymqttskill(MycroftSkill):
 
     def __init__(self):
         super(mymqttskill, self).__init__(name="mymqttskill")
-       
-        self.protocol = self.config["protocol"]
-	self.mqttssl = self.config["mqtt-ssl"]
-	self.mqttca = self.config["mqtt-ca-cert"]
-	self.mqtthost = self.config["mqtt-host"]
-	self.mqttport = self.config["mqtt-port"]
-	self.mqttauth = self.config["mqtt-auth"]
-	self.mqttuser = self.config["mqtt-user"]
-	self.mqttpass = self.config["mqtt-pass"]
- 
+
+        if ( not self.settings.get('protocol') ):
+           self.settings['protocol'] = 'mqtt'
+        if ( not self.settings.get('mqtthost') ):
+           self.settings['mqtthost'] = 'localhost'
+        if ( not self.settings.get('mqttport') ):
+           self.settings['mqttport'] = '1883'
     
     def initialize(self):
-        self.load_data_files(dirname(__file__))
-        self. __build_single_command()
-        
+        self.__build_single_command()
         
     def __build_single_command(self):
         intent = IntentBuilder("mymqttIntent").require("CommandKeyword").require("ModuleKeyword").require("ActionKeyword").build()
@@ -44,19 +38,20 @@ class mymqttskill(MycroftSkill):
         if act_name:
             cmd_name += '_' + act_name
 
-        if (self.protocol == "mqtt"):
-	    mqttc = mqtt.Client("MycroftAI")
-	    if (self.mqttauth == "yes"):
-	        mqttc.username_pw_set(self.mqttuser,self.mqttpass)
-	    if (self.mqttssl == "yes"):
-		mqttc.tls_set(self.mqttca) #/etc/ssl/certs/ca-certificates.crt
-            mqttc.connect(self.mqtthost,self.mqttport)
-	    mqttc.publish("/mycroft/" + cmd_name + "/" + dev_name + "/" + act_name, act_name)
-	    mqttc.disconnect()
-	    self.speak_dialog("cmd.sent")
-            LOGGER.info(dev_name + "-" + cmd_name)
+        if (self.settings['protocol'] == "mqtt"):
+            mqttc = mqtt.Client("MycroftAI")
+            if (self.settings.get('mqttuser')):
+                mqttc.username_pw_set(self.settings['mqttuser'],self.setting['mqttpass'])
+            if (self.settings.get('mqttca')):
+                mqttc.tls_set(self.settings['mqttca']) #/etc/ssl/certs/ca-certificates.crt
+            LOGGER.info( "MQTT Connect: " + self.settings['mqtthost'] + ':' + str(self.settings['mqttport']) )
+            mqttc.connect(self.settings['mqtthost'], self.settings['mqttport'])
+            mqttc.publish(cmd_name + "/" + dev_name + "/" + act_name, act_name)
+            mqttc.disconnect()
+            self.speak_dialog("cmd.sent")
+            LOGGER.info("MQTT Publish: " + cmd_name + "/" + dev_name + "/" + act_name)
 
-	else:
+        else:
             self.speak_dialog("not.found", {"command": cmd_name, "action": act_name, "module": dev_name})
             LOGGER.error("Error: {0}".format(e))
         
