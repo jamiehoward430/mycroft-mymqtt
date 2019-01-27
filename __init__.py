dfcc252e6fb481923839c567932ecb880286dd49
jgstratton <jgstratt@sdf.org>
2019-01-26 22:07:33 -0600

    Modified to work with my roomba serial skill.

--- __init__.py
+++ __init__.py
@@ -14,13 +14,14 @@ class mymqttskill(MycroftSkill):
 
     def __init__(self):
         super(mymqttskill, self).__init__(name="mymqttskill")
-
-        if ( not self.settings.get('protocol') ):
-           self.settings['protocol'] = 'mqtt'
         if ( not self.settings.get('mqtthost') ):
            self.settings['mqtthost'] = 'localhost'
         if ( not self.settings.get('mqttport') ):
            self.settings['mqttport'] = '1883'
+        self.mqttc = mqtt.Client("MycroftAI")
+
+    def shutdown(self):
+        self.mqttc.disconnect()
     
     def initialize(self):
         self.__build_single_command()
@@ -30,30 +31,24 @@ class mymqttskill(MycroftSkill):
         self.register_intent(intent, self.handle_single_command)
         
     def handle_single_command(self, message):
-        cmd_name = message.data.get("CommandKeyword")
-        mdl_name = message.data.get("ModuleKeyword")
-        act_name = message.data.get("ActionKeyword")
-        dev_name = mdl_name.replace(' ', '_')
+        cmd_name = message.data.get("CommandKeyword").replace(' ', '_')
+        dev_name = message.data.get("ModuleKeyword").replace(' ', '_')
+        act_name = message.data.get("ActionKeyword").replace(' ', '_')
         
-        if act_name:
-            cmd_name += '_' + act_name
-
-        if (self.settings['protocol'] == "mqtt"):
-            mqttc = mqtt.Client("MycroftAI")
+        try:
             if (self.settings.get('mqttuser')):
-                mqttc.username_pw_set(self.settings['mqttuser'],self.setting['mqttpass'])
+                self.mqttc.username_pw_set(self.settings['mqttuser'],self.setting['mqttpass'])
             if (self.settings.get('mqttca')):
-                mqttc.tls_set(self.settings['mqttca']) #/etc/ssl/certs/ca-certificates.crt
+                self.mqttc.tls_set(self.settings['mqttca']) #/etc/ssl/certs/ca-certificates.crt
             LOGGER.info( "MQTT Connect: " + self.settings['mqtthost'] + ':' + str(self.settings['mqttport']) )
-            mqttc.connect(self.settings['mqtthost'], self.settings['mqttport'])
-            mqttc.publish(cmd_name + "/" + dev_name + "/" + act_name, act_name)
-            mqttc.disconnect()
-            self.speak_dialog("cmd.sent")
-            LOGGER.info("MQTT Publish: " + cmd_name + "/" + dev_name + "/" + act_name)
+            self.mqttc.connect(self.settings['mqtthost'], self.settings['mqttport'])
 
-        else:
+            self.mqttc.publish(dev_name + "/" + cmd_name, act_name)
+            self.mqttc.disconnect()
+            self.speak_dialog("cmd.sent")
+            LOGGER.info("MQTT Publish: " + dev_name + "/" + cmd_name + "/" + act_name)
+        except:
             self.speak_dialog("not.found", {"command": cmd_name, "action": act_name, "module": dev_name})
-            LOGGER.error("Error: {0}".format(e))
         
     def stop(self):
         pass
